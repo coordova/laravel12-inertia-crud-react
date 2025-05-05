53beb6dfdcdadcb018430256d368b549153daa10
coordova <grcordova@gmail.com>
2025-05-05 19:15:03 -0400

    feat: Create initial index page for Posts with layout and head title

---
+++ resources/js/pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/layouts/app-layout'
+import { Head } from '@inertiajs/react'
+
+export default function index.tsx({/*|*/}) {
+    return (
+        <Layout>
+            <Head title="index.tsx"/>
+
+        </Layout>
+    )
+}
