d93044004beb92b2416bcf8fd4125713939371b6
coordova <grcordova@gmail.com>
2025-05-05 19:18:05 -0400

    feat: Enhance Posts functionality with Inertia.js integration and routing

--- resources/js/pages/posts/index.tsx
+++ resources/js/pages/posts/index.tsx
@@ -1,11 +1,18 @@
 import Layout from '@/layouts/app-layout'
 import { Head } from '@inertiajs/react'
+import { BreadcrumbItem } from '@/types';
 
-export default function index.tsx({/*|*/}) {
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Posts',
+        href: '/posts',
+    },
+]
+export default function index({}) {
     return (
-        <Layout>
-            <Head title="index.tsx"/>
-
+        <Layout breadcrumbs={breadcrumbs}>
+            <Head title="Posts"/>
+            <h1>index</h1>
         </Layout>
     )
 }
